2f7fd6e27b8ff19509d87edfef85c55893ca88ce
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-17 09:44:49 -0800

    [Swetha] add : json data managment  program

---
+++ ObjectOrientedProgram/ObjectOrientedProgram/InventoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgram
+{
+    class InventoryModel
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+
+    }
+}
